f22e5b2d6b64b25bbb6e3f44b2c68140032a4493
CesarRobertopg <cesar.robert1996@gmail.com>
2021-01-26 19:15:35 -0300

    pequenas mudanças no código e adição do eslint

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import styled from "styled-components";
-import db from "../db.json";
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
 import Widget from '../src/components/Widget/index';
 import Footer from '../src/components/Footer/index';
 import GitHubCorner from '../src/components/GitHubCorner/index';
 import QuizBackground from '../src/components/QuizBackground/index';
+import QuizLogo from '../src/components/QuizLogo/index';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -12,7 +17,7 @@ import QuizBackground from '../src/components/QuizBackground/index';
 //   background-position: center;
 // `;
 
-export const QuinzContainer = styled.div`
+export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -23,32 +28,52 @@ export const QuinzContainer = styled.div`
   }
 `;
 
-
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
-      <QuinzContainer>
+      <Head>
+        <title>Alura Quiz - Pokémon!</title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Quiz Em Construção</h1>
           </Widget.Header>
           <Widget.Content>
             <p>Aguarde enquanto preparamos um ótimo quiz para você!</p>
-            <input type="text" placeholder="Diz aí seu nome pra jogar :)"></input>
-            <button >JOGAR</button>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={(event) => {
+                  // State
+                  // name = event.target.value;
+                  setName(event.target.value);
+                }}
+                placeholder="Diz aí seu nome pra jogar :)"
+              />
+              {/* eslint-disable-next-line react/button-has-type */}
+              <button type="submit" disabled={name.length === 0}>
+                JOGAR
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Content>
             <h1>Futuros Quizes da Galera</h1>
             <p>Aqui terão vários quizes super interessantes dos outros alunos, aguarde!</p>
-            <ul></ul>
           </Widget.Content>
         </Widget>
         <Footer />
-      </QuinzContainer>
-      <GitHubCorner projectUrl="https://github.com/CesarRobertopg"/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/CesarRobertopg" />
     </QuizBackground>
   );
 }
---
+++ pages/quiz.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+export default function Quizpage() {
+  return (
+    <div>
+      Página de Quiz!
+    </div>
+  );
+}
